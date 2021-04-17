8fd600bbc4c0ac4ba0175ca18cddba8105d96964
joeljdv <joeldiazjdv@gmail.com>
2021-04-17 15:46:07 -0400

    commit tree c014e0b7b14e7b096362d1dfba7230729e66ad5b

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+    
+    handleClick = (e) => {
+        let array = [e.clientX, e.clientY]
+        this.props.onReceiveCoordinates(array)
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick} />
+            </div>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+    setTimeout = () => {
+        this.props.delay
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={onDelayedClick(this.setTimeout)}/>
+            </div>
+        )
+    }
+}
